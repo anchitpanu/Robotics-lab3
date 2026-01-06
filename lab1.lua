3273e67f0af623eef8ae94c0cfe3dcd595f4d111
JOR <153255837+jorjor140900@users.noreply.github.com>
2026-01-06 09:56:17 +0700

    Create lab1.lua

---
+++ lab1.lua
@@ -0,0 +1,54 @@
+SpeedFactor(15)
+Go(InitialPose)
+
+--pic1
+MovJ(P1)
+MovL(P2)
+MovL(P3)
+MovL(P4)
+MovL(P5)
+MovL(P6)
+
+--pic5
+Go(InitialPose)
+MovJ(P1)
+MovL(P2)
+MovL(P13)
+Arc(P11, P8)
+MovL(P9)
+Arc(P12, P10)
+MovL(P5)
+MovL(P6)
+
+--pic2
+Go(InitialPose)
+MovJ(P31)
+MovL(P3)
+Arc(P14, P15)
+Arc(P16, P17)
+MovL(P18)
+MovL(P3)
+
+--pic3
+Go(InitialPose)
+MovJ(P32)
+MovL(P15)
+Arc(P19, P4,"CP=100")
+Arc(P20, P15,"CP=100")
+
+--pic4
+Go(InitialPose)
+MovJ(P33)
+MovL(P21)
+MovL(P22)
+MovL(P23)
+MovL(P24)
+MovL(P25)
+MovL(P26)
+MovL(P27)
+MovL(P28)
+MovL(P29)
+MovL(P30)
+MovL(P21)
+
+Go(InitialPose)
