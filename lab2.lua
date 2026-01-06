ca8bcb1811a8cb8318c16e6a6b8d1e11689922a7
anchitpanu <152593940+anchitpanu@users.noreply.github.com>
2026-01-06 09:41:07 +0700

    lab2.lua

---
+++ lab2.lua
@@ -0,0 +1,28 @@
+--lab2: bring the box from this point to another point 
+
+Go(InitialPose)
+DO(7, OFF)
+DO(8, OFF)
+Wait(2000)
+
+MovL(P1)
+MovJ(RelPoint(P2,{0,0,27,0}))
+DO(7,ON)
+Wait(1000)
+MovL(P1)
+MovL(P3)
+MovL(P4)
+DO(7, OFF)
+DO(8,ON)
+Wait(1000)
+DO(8,OFF)
+MovL(P3)
+MovL(InitialPose)
+
+MovL(P1)
+MovL(P2)
+
+
+
+
+
